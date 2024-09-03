22e17bfe7f7f2a15e063cbcbbb68c18c42e0a300
guilopes2020 <guilopesdev@gmail.com>
2024-09-03 10:19:17 -0300

    feat: :sparkles: first-commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiVideo.Models;
+
+namespace WebApiVideo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<AutorModel> Autores { get; set; }
+    public DbSet<LivroModel> Livros { get; set; }
+}
---
+++ Models/LivroModel.cs
@@ -0,0 +1,8 @@
+namespace WebApiVideo.Models;
+
+public class LivroModel
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public AutorModel Autor { get; set; }
+}
